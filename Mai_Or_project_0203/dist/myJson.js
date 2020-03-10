3df5f341fee332bcec626901d453b321a81a6341
maipichhdze <37333124+maipichhdze@users.noreply.github.com>
2020-03-10 22:06:54 +0200

    Add files via upload

---
+++ Mai_Or_project_0203/dist/myJson.js
@@ -0,0 +1,16 @@
+var myArray = [
+    { "myH1":"שינוי כיוון", "myShortcut":"F1", "MyExplain":"לשינוי כיוון המטוס ניתן ללחוץ עליו ב Data block ולאחר מכן נקליד את הערך הרצוי. ניתן גם למתוח קו מתוך המטוס לנקודה מסוימת.","myImg":"./images/altitude.png" },
+    { "myH1":"שינוי גובה", "myShortcut":"F2", "MyExplain":"לשינוי גובה המטוס ניתן ללחוץ עליו ב Data block ולאחר מכן נקליד את הערך הרצוי.","myImg":"./images/heading.png" },
+    { "myH1":"שינוי מהירות", "myShortcut":"F3", "MyExplain":"לשינוי מהירות המטוס ניתן ללחוץ עליה ב Data block, ולאחר מכן נקליד את הערך הרצוי.","myImg":"./images/altitude.png" },
+    { "myH1":"שינוי זעט", "myShortcut":"F4", "MyExplain":"לשינוי מספר הזעט ניתן ללחוץ עליו ב Data block ולאחר מכן נקליד את הערך הרצוי.","myImg":"./images/altitude.png" },
+    { "myH1":"הוספה/הסרה של טבעות מרחק", "myShortcut":"אין", "MyExplain":"הוספה של טבעות מרחק בהתאם לסביבת העבודה המוגדרת. מעיגול לעיגול 20 מייל.","myImg":"./images/rings.png" },
+    { "myH1":"עריכת הגדרות תצוגה", "myShortcut":"אין", "MyExplain":"Display - שינוי תצוגה, למשל הגדרת מרחק בין טבעות. Targets - שינוי ההגדרות של המטרות המופיעות על המסך, למשל שינוי צבעים. ","myImg":"./images/color_prop.png" },
+    { "myH1":"איתור מטרה (מטוס)", "myShortcut":"אין", "MyExplain":"איתור של מטוס על המסך באמצעות אות קריאה או זעט. לאחר החיפוש המטוס יסומן הצהוב.","myImg":"./images/find.png" },
+    { "myH1":"מטוסים פעילים", "myShortcut":"אין", "MyExplain":"הצגת כל המטוסים הפעילים בסימולציה. מאפשר לערוך את הנתונים שלהם.","myImg":"./images/active.png" },
+    { "myH1":"רשימת המראות עתידיות", "myShortcut":"אין", "MyExplain":"הצגת רשימת כל ההמראות העתידיות ומטוסים שבאוויר.","myImg":"./images/future.png" },
+    { "myH1":"שורת פקודות - Command Line", "myShortcut":"אין", "MyExplain":"בחלון זו מתועדות כל הפקודות שניתנו למטוס הנבחר.","myImg":"./images/command.png" },
+    { "myH1":"חלון משימות - ETDL", "myShortcut":"אין", "MyExplain":"רשימת אירועי הסימולציה. משמעות הצבעים ברשימה: ירוק - מצב התחלתי. אדום - לביצוע. כחול - לידיעה.","myImg":"./images/prompt.png" },
+    { "myH1":"סימון מטוס - Halo", "myShortcut":"אין", "MyExplain":"באמצעות קליק ימני על המטוס ובחירה האפשרות Halo מהתפריט. המטוס יובלט באמצעות עיגול מסביבו.","myImg":"./images/halo.png" },
+    { "myH1":"העברת שליטה על מטוס - Handoff", "myShortcut":"אין", "MyExplain":"העברת מטוס מסוים לתחום האחריות של מסטיס אחר במהלך הסימולציה. קליק ימני על המטוס ובחירה ב-Handoff initiate","myImg":"./images/handoff.png" },
+    { "myH1":"מיקום מחדש של מטוס", "myShortcut":"אין", "MyExplain":"מיקום מחדש של מטוס לפי נ.צ שינתן, כיוון, גובה ומהירות. מאפשר למקם את המטוס בכל מקום אחר במרחב. קליק ימני על המטוס ובחירה ב-Reposition target.","myImg":"./images/reposition.png" }
+];
